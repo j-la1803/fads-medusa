8127a02c3353916a4352e444615f590d06b6a4f0
Justin LaFountain <jlafountain@fredastaire.com>
2026-07-07 15:35:39 -0500

    Initial FADS Demo

---
+++ apps/storefront/src/app/[countryCode]/(main)/page.tsx
@@ -0,0 +1,49 @@
+import { Metadata } from "next"
+
+import FeaturedProducts from "@modules/home/components/featured-products"
+import Hero from "@modules/home/components/hero"
+import { listCollections } from "@lib/data/collections"
+import { getRegion } from "@lib/data/regions"
+
+export const metadata: Metadata = {
+  title: "Medusa Next.js Starter Template",
+  description:
+    "A performant frontend ecommerce starter template with Next.js 15 and Medusa.",
+}
+
+
+export default async function Home(props: {
+  params: Promise<{ countryCode: string }>
+}) {
+  const params = await props.params
+
+  const { countryCode } = params
+
+  const region = await getRegion(countryCode)
+
+  const { collections } = await listCollections({
+    fields: "id, handle, title",
+  })
+
+  if (!collections || !region) {
+    return null
+  }
+
+  return (
+    <><section className="py-12 px-6 text-center bg-gray-100">
+  <h1 className="text-4xl font-bold mb-4">
+    FADS Medusa Storefront Proof of Concept
+  </h1>
+  <p className="text-lg text-gray-600">
+    Testing Medusa as a commerce engine for Fred Astaire Dance Studios.
+  </p>
+</section>
+      <Hero />
+      <div className="py-12">
+        <ul className="flex flex-col gap-x-6">
+          <FeaturedProducts collections={collections} region={region} />
+        </ul>
+      </div>
+    </>
+  )
+}
